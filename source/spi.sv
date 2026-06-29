62bfd0f2ad588dba528207a04a87b74b9385169f
TCinCA173821 <chan480@purdue.edu>
2026-06-29 10:51:02 -0400

    instantiate spi top module

---
+++ source/spi.sv
@@ -0,0 +1,11 @@
+module spi_slave(
+    input logic MOSI[7:0],
+    input logic CS,
+    input logic SCLK
+    input logic result[4:0],
+    input logic infdone,
+    output logic infdone,
+    output logic datavalid
+);
+    
+endmodule
