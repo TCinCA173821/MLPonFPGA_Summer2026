ac653382d5a6283e8fe83b399f2fdfe23a3b2d15
TCinCA173821 <chan480@purdue.edu>
2026-06-29 12:14:57 -0400

    imlemented 2 flip flop sync

---
+++ source/spi_image_system/spi_top.sv
@@ -0,0 +1,12 @@
+module spi_top(
+    input logic MOSI[7:0],
+    input logic CS,
+    input logic SCLK,
+    input logic result[4:0],
+    input logic infdone,
+    output logic datavalid,
+    output logic spiout[15:0],
+    output logic MISO
+);
+    
+endmodule
