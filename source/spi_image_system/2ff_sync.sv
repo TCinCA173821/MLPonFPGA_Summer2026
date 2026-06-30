52f9663dee959afc96bab01c80e050115e190778
TCinCA173821 <chan480@purdue.edu>
2026-06-30 15:15:26 -0400

    implemented 2ff synchronizer as separate module from synchronizer top

---
+++ source/spi_image_system/2ff_sync.sv
@@ -0,0 +1,19 @@
+module ff2(
+    input logic rst_n,
+    input logic clk,
+    input logic in,
+    output logic out,
+);
+    logic ff1, ff2;
+
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            ff1 <= 1'b0;
+            ff2 <= 1'b0;
+        end else begin   
+            ff1 <= in;
+            ff2 <= ff1;
+        end
+    end
+    assign out = ff2;
+endmodule
