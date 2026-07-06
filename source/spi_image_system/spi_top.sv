1caa82b8c9c07a8f369b1d7d8ed78b87309122d4
TCinCA173821 <chan480@purdue.edu>
2026-07-06 14:52:11 -0400

    fixed syntax errors

--- source/spi_image_system/spi_top.sv
+++ source/spi_image_system/spi_top.sv
@@ -1,12 +1,12 @@
-module spi_top(
-    input logic MOSI[7:0],
-    input logic cs_in,
-    input logic SCLK,
-    input logic result[4:0],
-    input logic infdone,
+module spi_fsm(
+    input logic mosi[7:0],
+    input logic cs,
+    input logic sclk,
     output logic datavalid,
     output logic spiout[15:0],
-    output logic MISO
+    
 );
     
+
+
 endmodule
