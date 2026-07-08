96692b458298e76eed192fc590d91211e37891bc
AshishDhillon2 <dhillo19@purdue.edu>
2026-07-08 15:44:02 -0400

    Update parallel_bus_config.h
    renamed macros

--- pico_to_fpga/src/parallel_bus_config.h
+++ pico_to_fpga/src/parallel_bus_config.h
@@ -2,24 +2,20 @@
 #define PARALLEL_BUS_CONFIG_H
 
 // active-high chip select
-#define PARALLEL_BUS_CS_ACTIVE_HIGH 1
+#define BUS_CS_ACTIVE_HIGH 1
 
+#define BUS_D0_PIN 2u
+#define BUS_D1_PIN 3u
+#define BUS_D2_PIN 4u
+#define BUS_D3_PIN 5u
+#define BUS_D4_PIN 6u
+#define BUS_D5_PIN 7u
+#define BUS_D6_PIN 8u
+#define BUS_D7_PIN 9u
 
-#define PARALLEL_BUS_DATA_PINS { \
-    2u, \
-    3u, \
-    4u, \
-    5u, \
-    6u, \
-    7u, \
-    8u, \
-    9u  \
-}
+#define BUS_SCLK_PIN 10u
+#define BUS_CS_PIN   11u
 
-#define PARALLEL_BUS_SCLK_PIN 10u
-#define PARALLEL_BUS_CS_PIN   11u
-
-
-#define PARALLEL_BUS_DELAY_US 1u
+#define BUS_DELAY_US 1u
 
 #endif
