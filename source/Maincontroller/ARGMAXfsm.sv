15ee6cfc6101648cfa856e65426eea81caf394ac
tibnel <160969029+tibnel@users.noreply.github.com>
2026-07-13 16:49:53 -0400

    reworked argmax fsm

--- source/Maincontroller/ARGMAXfsm.sv
+++ source/Maincontroller/ARGMAXfsm.sv
@@ -1,50 +1,73 @@
-module ARGMAXfsm(
-    input logic clk,
-    input logic n_rst,
-    input logic ARGen,
-    input logic ARGdv,
-    output logic ARGd,
-    output logic OLren,
-    output logic ARGmax_start
+module argmax_controller (
+	input logic clk,
+	input logic nrst,
+	input logic arg_en,
+	input logic arg_dv,
+	input logic [3:0] node,
+	output logic arg_done,
+	output logic buffer_inc;
+	output logic arg_start
 );
 
-    typedef enum logic [1:0] { 
-        IDLE,
-        READ,
-        COMPARE
-    } state_t;
-    state_t curstate, nxtstate;
-    logic [3:0] node, nxtnode;
-    always_ff @ (posedge clk, negedge n_rst) begin
-        if(!n_rst) begin
-            curstate <= IDLE;
-            node <= 'd0;
-        end else begin
-            curstate <= nxtstate;
-            node <= nxtnode;
-        end
-    end
+//states
+typedef enum logic [1:0] {
+	IDLE,
+	RUN,
+	INCR,
+	DONE
+} state_t;
+state_t state, next_state;
 
-    always_comb begin
-        case(curstate)
-            IDLE: nxtstate = ARGen ? READ : IDLE;
-            READ: nxtstate = COMPARE;
-            COMPARE: nxtstate = ARGdv ? (node == 'd9) ? IDLE: READ: COMPARE;
-            default: nxtstate = IDLE;
-        endcase
-    end
-    always_comb begin
-        ARGd = 'd0;
-        OLren = 'd0;
-        ARGmax_start = 'd0;
-        nxtnode = node;
-        case(curstate)
-            READ: OLren = 'd1;
-            COMPARE: begin 
-                ARGmax_start = 'd1;
-                nxtnode = node + 'd1;
-            end
-        endcase
-    end
+//state change logic
+always_comb begin
+	case(state)
+		IDLE: next_state = arg_en ? RUN : IDLE;
+		RUN: next_state = INCR;
+		INCR: next_state = (node == 'd9) ? DONE : RUN;
+		DONE: next_state = IDLE;
+		default: next_state = IDLE;
+		end
+	endcase
+end
+
+//state and node num changes
+always_ff @(posedge clk, negedge nrst) begin
+	if(nrst) begin
+		node <= '0;
+		state <= IDLE;
+	end else begin
+		node <= next_node;
+		state <= next_state;
+	end
+end
+
+//signal updates
+always_comb begin
+	arg_done = '0;
+	buffer_inc = '0;
+	arg_start = '0;
+
+	case(state)
+		IDLE: begin
+            arg_done = '0;
+            buffer_inc = '0;
+            arg_start = '0;
+        end
+        RUN: begin
+			arg_start = '1;
+			buffer_inc = '0;
+			arg_done = '0;
+		end
+		INCR: begin
+			buffer_inc = '1;
+			arg_start = '0;
+			arg_done = '0
+		end
+		DONE: begin
+			arg_done = '1;
+			buffer_inc = '0;
+			arg_start = '0;
+		end
+	endcase
+end
 endmodule
-    
