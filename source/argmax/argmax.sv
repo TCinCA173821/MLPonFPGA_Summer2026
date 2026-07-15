38ddb046ade1ca9302768cdeb948d375f36de0dd
NoahZ34 <124742989+NoahZ34@users.noreply.github.com>
2026-07-15 10:09:03 -0400

    Update argmax.sv
    fixed reg width

--- source/argmax/argmax.sv
+++ source/argmax/argmax.sv
@@ -2,12 +2,12 @@ module argmax (
 	input logic clk,
 	input logic nrst,
 	input logic start,
-	input logic [11:0] in,
+	input logic [15:0] in,
 	input logic [3:0] in_ptr,
 	output logic [3:0] out,
 );
 
-logic [11:0] out_reg;
+	logic [15:0] out_reg;
 logic load_en;
 
 //register
