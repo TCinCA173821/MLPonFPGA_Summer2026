a46aac651caa1217123227ab6c166e17c2a68d90
NoahZ34 <124742989+NoahZ34@users.noreply.github.com>
2026-07-16 14:09:09 -0400

    Update MACmodule.sv
    fixed sign handling and assignement

--- source/mac/MACmodule.sv
+++ source/mac/MACmodule.sv
@@ -3,7 +3,7 @@ module mixedsign4bitmult(
     input logic [3:0] unsignedin2,
     output logic signed [15:0] signextendedout
 );
-    assign signextendedout = signed'(signedin1) * signed'({1'b0,unsignedin2});
+    assign signextendedout = $signed(signedin1) * $signed({1'b0,unsignedin2});
 endmodule
 
 module addersigned16bit(
@@ -20,18 +20,18 @@ module accreg(
     input logic wen,
     input logic len,
     input logic signed [15:0] in,
-    input logic signed [7:0] Lin,
+    input logic [7:0] Lin,
     output logic signed [15:0] out
 );
 
     logic signed [15:0] reg_val, reg_val_nxt;
     always_ff @ (posedge clk, negedge n_rst) begin
-        if(!n_rst) reg_val <= 'd0;
+        if(!n_rst) reg_val <= 16'd0;
         else reg_val <= reg_val_nxt;
     end
 
     always_comb begin
-        if (len) reg_val_nxt = signed'(Lin);
+        if (len) reg_val_nxt = $signed(Lin);
         else if(wen) reg_val_nxt = in;
         else reg_val_nxt = reg_val;
     end
@@ -60,7 +60,7 @@ module MAC(
 );
     logic signed [15:0] mult_out, reg_in;
     
-    mixedsign4bitmult mult1 (.signedin1(MAC_in[7:4]),.unsignedin2(MAC_in[3:0]),.signextendedout(mult_out));
+    mixedsign4bitmult mult1 (.signedin1($signed(MAC_in[7:4])),.unsignedin2(MAC_in[3:0]),.signextendedout(mult_out));
     addersigned16bit adder1 (.in1(mult_out),.in2(MAC_out),.out(reg_in));
     accreg buf_reg1 (.clk(clk),.n_rst(n_rst),.wen(MAC_s),.len(MAC_l),.in(reg_in),.Lin(MAC_in),.out(MAC_out));
     relu relu1 (.in(MAC_out),.out(MAC_outrelu));
