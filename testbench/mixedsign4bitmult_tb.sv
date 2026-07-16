75ddb33984c77cd9bc3749c0f181fe06e9020cec
tibnel <160969029+tibnel@users.noreply.github.com>
2026-07-16 17:31:46 -0400

    finished tb
    Added a testbench for mixed-sign 4-bit multiplier with waveform dumping and test result display.

--- testbench/mixedsign4bitmult_tb.sv
+++ testbench/mixedsign4bitmult_tb.sv
@@ -10,3 +10,25 @@ module mixedsign4bitmult_tb;
   mixedsign4bitmult DUT (
     .*;
   );
+
+  initial begin
+    $dumpfile("waveform.fst");
+    $dumpvars(0, mixedsign4bitmult_tb);
+    
+    for (int i = -8; i < 8; i++) begin
+      for (int j = 0; j < 16; j++) begin
+        signedin1 = 4'(i);
+        unsignedin2 = 4'(j);
+        #(1);
+
+        total_tests++;
+        if(signextendedout == signed'(4'(i)) * signed'({1'b0, 4'(j)}) begin
+          tests_passed++;
+        end
+      end
+    end
+    $display("%d/%d tests passed", tests_passed, total_tests);
+           
+    $finish
+  end
+endmodule
