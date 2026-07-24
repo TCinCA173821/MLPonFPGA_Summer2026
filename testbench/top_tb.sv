6a6ed522c993b3319c13768bb51b5739401e51de
tibnel <160969029+tibnel@users.noreply.github.com>
2026-07-23 21:17:42 -0400

    draft

--- testbench/top_tb.sv
+++ testbench/top_tb.sv
@@ -1,4 +1,4 @@
-module tob_tb;
+module top_tb;
   logic hz100, reset;
   logic [20:0] pb;
   logic [7:0] left, right, ss7, ss6, ss5, ss4, ss3, ss2, ss1, ss0;
@@ -14,32 +14,32 @@ module tob_tb;
   int cycles = 0;
   int i = 0;
   logic finished;
-  logic[7:0] test_biases [0:25]
+  logic[7:0] test_biases [0:25];
 
-  task reset();
+  task rst();
     reset = 1'b1;
-    repeat(2) @(posedge clk);
+    repeat(2) @(posedge hz100);
     reset = 1'b0;
-    @(posedge clk);
+    @(posedge hz100);
     #(1);
   endtask
 
   task send_data(
-    input logic [7:0] test_input,
-    input logic [3:0] expected
+    input logic [7:0] test_input
   );
     pb[10] = 1'b1;
     pb[19:12] = test_input;
-    @(posedge SCLK);
+    @(posedge pb[11]);
     #(1);
     pb[10] = 1'b0;
   endtask
   
   task test(
-    input logic[7:0] test_biases [0:25]
+    input logic[7:0] test_data [0:25],
+	input logic [3:0] expected
   );
     pb[9] = 1'b1;
-    @(posedge clk);
+    @(posedge hz100);
     #(1);
     pb[9] = 1'b0;
 
@@ -54,28 +54,28 @@ module tob_tb;
       if(left[0]) finished = 1'b1;
     end
 
-    if(finished && left[5:2] == out) begin
-      $display("passed: expected: %d, out: %d", expected, out);
+    if(finished && (left[5:2] == expected)) begin
+      $display("passed: expected: %d, out: %d", expected, left[5:2]);
     end else begin
-      $display("failed: expected: %d, out: %d", expected, out);
+      $display("failed: expected: %d, out: %d", expected, left[5:2]);
     end
   endtask
   
   initial begin
     $dumpfile("waveform.fst");
     $dumpvars(0, top_tb.sv);
-    n_rst = 1'b1;
+    reset = 1'b0;
     $timeformat(-9, 2, " ns", 20);
-    reset();
-    @(posedge clk);
+    rst();
+    @(posedge hz100);
     #(10); 
 
     for(int i = 0; i < 26; i++) begin
       test_biases[i] = 1'd1;
     end
-    test_biases[25] = 1'd100;
+    test_biases[25] = 8'd100;
 
-    test(test_biases, 3'd9);
+    test(test_biases, 4'd9);
     
     $finish();
   end
